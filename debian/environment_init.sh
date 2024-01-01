3c5ec9f9c52fe0c49a544dd560a02d3fc5f1a481
lipe <62669782+lipeeeee@users.noreply.github.com>
2024-01-01 01:03:47 +0000

    Create environment_init.sh

---
+++ debian/environment_init.sh
@@ -0,0 +1,22 @@
+# Jetbrains Mono Nerd Font 
+
+sudo apt update && sudo apt upgrade
+sudo apt install python3-pip
+sudo apt install unzip
+sudo apt install npm
+pip3 install poetry
+
+# Lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+# Latest neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+# Optional: exposing nvim globally.
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
