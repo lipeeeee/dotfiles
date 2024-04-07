ac800311a2d7e34bd5573bd9afa2e960a179853c
lipeeeee <filipe.pereirafga@gmail.com>
2024-04-07 23:56:52 +0100

    init environment workdir

--- scripts/init_environment.sh
+++ scripts/init_environment.sh
@@ -2,28 +2,35 @@
 # Jetbrains Mono Nerd Font 
 
 echo "Installing apt packages..."
-sudo apt update && sudo apt upgrade
+apt update && sudo apt upgrade
 declare -a aptinstall=("python3-pip" "unzip" "npm" "ripgrep" "clangd" "tree" "tmux" "fzf" "curl" "git" "vim")
-sudo apt install "${aptinstall[@]}"
+apt install "${aptinstall[@]}"
 
 echo "Installing pip packages..."
 declare -a pipinstall=("poetry")
 pip3 install "${pipinstall[@]}"
 
+# Create work dir
+workdir="./tmp/"
+mkdir $workdir
+
 echo "Installing Lazygit..."
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
+curl -o $workdir/lazygit.tar.gz -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf $workdir/lazygit.tar.gz $workdir/lazygit
+install $workdir/lazygit /usr/local/bin
 
 echo "Installing latest neovim build..."
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-./nvim.appimage --appimage-extract
-./squashfs-root/AppRun --version
+curl -o $workdir/nvim.appimage -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x $workdir/nvim.appimage
+./$workdir/nvim.appimage --appimage-extract
+./$workdir/squashfs-root/AppRun --version
 # Optional: exposing nvim globally.
-sudo mv squashfs-root /
-sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+mv $workdir/squashfs-root /
+ln -s $workdir/squashfs-root/AppRun /usr/bin/nvim
 
 # Import scripts to /usr/bin
 # TODO
+
+# Clean env
+# rm -rf $workdir
