e43fd7ae621e6fe2e66f82203a1bb19180015a7a
lipeeeee <filipe.pereirafga@gmail.com>
2025-04-16 20:46:35 +0100

    updated docker install

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
+# Must be run with sudo -E
+# Docker instalation Ubuntu 22.04 wsl 2
+# Might work on other debian based systems
 
-# Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+# 1. System update
+sudo apt update
+sudo apt upgrade -y
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+# 2. Dependencies
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 
-# install
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+# 3. GPG docker key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
+# 4. Add docker repo to system
+echo "deb [signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# 5. Install
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+# Add user to docker grp 
+# Warning : IF NOT RUN WITH sudo -E THIS WONT WORK
+sudo usermod -aG docker $USER
